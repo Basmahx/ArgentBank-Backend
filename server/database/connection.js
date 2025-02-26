5c89069cf93cfee1fa03c11b7f52a4b6ae78bcf9
Basmahx <138460878+Basmahx@users.noreply.github.com>
2025-02-26 16:10:58 +0100

    j'ai installé l'app

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL ||
+  "mongodb+srv://Basmah:NzqKz4ANVHm@cluster48585.ibw4p.mongodb.net/argentBankDB?retryWrites=true&w=majority";
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`❌ Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
